592658d17978350b1837167430815a639382f047
Callum-Swan <2195585s@student.gla.ac.uk>
2019-02-02 19:36:25 +0000

    Chapter 7

--- rango/forms.py
+++ rango/forms.py
@@ -18,8 +18,19 @@ class PageForm(forms.ModelForm):
 
     views=forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+            return cleaned_data
+
+    
     class Meta:
         model= Page
 
         exclude=("category",)
-        
+ 
