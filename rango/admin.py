1ac0b61fc812ef02cb37b82a674f0023d0ef406d
Callum-Swan <2195585s@student.gla.ac.uk>
2019-02-04 23:45:25 +0000

    Chapter 9.6/7

--- rango/admin.py
+++ rango/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from rango.models import UserProfile
 # Register your models here.
 from django.contrib import admin
 from rango.models import Category, Page
